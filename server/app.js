896cc99b72c3635f939caa2a829c2228bc15fca7
Firas Al-Houssari <f.alhusari@gmail.com>
2019-08-26 01:46:13 +0300

    End of course

--- server/app.js
+++ server/app.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
+//allow cross-origin requests
+app.use(cors());
+
+//connect to mlab database
+mongoose.connect('mongodb+srv://firas:123qwe@cluster0-ivveo.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
+//MongoClient.connect('mongodb+srv://firas:123qwe@cluster0-ivveo.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
+
 app.use('/graphql', graphqlHTTP({
-  
+  schema,
+  graphiql: true
 }));
 
 app.listen(4000, () => {
